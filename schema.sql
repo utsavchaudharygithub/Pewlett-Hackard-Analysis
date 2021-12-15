e2ad9429d6804719db454fc17794e99894f86a94
SHERPA 944allview CHAUDHARY <944allview@SHERPAs-iMac.hsd1.ca.comcast.net>
2021-12-14 18:54:03 -0800

    import datas on tables

--- schema.sql
+++ schema.sql
@@ -8,8 +8,8 @@ CREATE TABLE departments (
 SELECT * FROM departments;
 
 
-CREATE TABLE employees (
-     emp_no INT NOT NULL,
+CREATE TABLE employees(
+     	emp_no INT NOT NULL,
      birth_date DATE NOT NULL,
      first_name VARCHAR NOT NULL,
      last_name VARCHAR NOT NULL,
@@ -42,19 +42,22 @@ CREATE TABLE salaries (
 SELECT * FROM salaries;
 
 CREATE TABLE dept_emp (
-    emp_no INT,
-	dept_no INT,
-	from_date DATE,
-	to_date DATE,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (emp_no)
+emp_no INT NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
 );
+SELECT * FROM dept_emp;
 
-CREATE TABLE titles
-   emp_no INT,
-   title VARCHAR (10),
-   from_date DATE,
-   to_date DATE,
-   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-   PRIMARY KEY (emp_no)
+CREATE TABLE titles (
+    emp_no INT NOT NULL,
+    title VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no, title, from_date)
 );
+SELECT * FROM titles;
