bbdb714ed1e8f3fc678288e20f70086f255255f5
SHERPA 944allview CHAUDHARY <944allview@SHERPAs-iMac.hsd1.ca.comcast.net>
2021-12-17 00:17:18 -0800

    adding csv

---
+++ queries.sql
@@ -0,0 +1,43 @@
+-- The Number of Retiring Employees by Title
+SELECT em.emp_no, 
+        em.first_name, 
+        em.last_name,
+	    ti.title, 
+        ti.from_date, 
+        ti.to_date
+	INTO retiring_emp_by_title
+	FROM employees AS em
+	LEFT JOIN titles as ti
+	ON (em.emp_no = ti.emp_no)
+	WHERE (em.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	ORDER BY em.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no, 
+	first_name, 
+    last_name, 
+    title
+INTO unique_titles
+FROM retiring_emp_by_title
+ORDER BY emp_no ASC, to_date DESC;
+
+
+-- retrieve the number of employees by their most recent job title  who are about to retire.
+SELECT COUNT(title), title 
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+
+-- query to create a Mentorship Eligibility table that holds the employees who are eligible to participate in a mentorship program.
+SELECT DISTINCT ON (em.emp_no) em.emp_no, em.first_name, em.last_name, em.birth_date, de.from_date, de.to_date, ti.title
+INTO mentorship_elegible 
+FROM employees AS em
+LEFT JOIN department_employee AS de
+ON em.emp_no = de.emp_no
+LEFT JOIN titles AS ti
+ON em.emp_no = ti.emp_no
+WHERE (em.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY em.emp_no;
